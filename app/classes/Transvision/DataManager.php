c2a6d228ae727fa9d58a2a9b0583b41df8605be0
Théo Chevalier <theo.chevalier11@gmail.com>
2016-04-12 01:08:38 +0200

    Check files against a list of dirs

--- app/classes/Transvision/DataManager.php
+++ app/classes/Transvision/DataManager.php
@@ -3,18 +3,17 @@ namespace Transvision;
 
 use Bit3\GitPhp\GitException;
 use Bit3\GitPhp\GitRepository;
-use Github\Client;
 use Monolog\Handler\ErrorLogHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Logger;
 
-
 /**
  * @package Transvision
  */
-class DataMan extends \VCS\Git {
-
-    public function __construct($path) {
+class DataManager extends \VCS\Git
+{
+    public function __construct($path)
+    {
         parent::__construct($path);
         // We use the Monolog library to log our events
         $this->logger = new Logger('DataManager');
@@ -39,9 +38,18 @@ class DataMan extends \VCS\Git {
             // Add files to git index then commit
             $this->git->add()->all()->execute();
             $this->git->commit()->message($commit_msg)->author($author)->date($date)->execute();
-
         } catch (GitException $e) {
             $this->logger->error('Failed to commit to Git repository. Error: '
+                                 . $e->getMessage());
+        }
+    }
+
+    public function status()
+    {
+        try {
+            return $this->execute('git status --short');
+        } catch (GitException $e) {
+            $this->logger->error('Failed to check status in Git repository. Error: '
                                  . $e->getMessage());
         }
     }
